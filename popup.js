9f1f3a11e6c45771d0fdd118ebf1b77bd81d07f6
MIDACO <thanhdatphukienbep@gmail.com>
2016-03-30 19:29:05 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://giaiphapthuonghieu.vn/daotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html?utm_source=google&utm_medium=Kh%C3%B4ng%20ph%E1%BA%A3i%20tr%E1%BA%A3%20ti%E1%BB%81n&utm_campaign=ads_trackling&utm_term=seo&utm_content=seo' target='_blank'><img src='https://jgoogle.googlecode.com/svn/trunk/dao-tao-seo-chuyen-sau.jpg' width='500px'/>ÄÃ o táº¡o seo chuyÃªn sÃ¢u</a>. Há»c táº¡i nhÃ  Linh Nguyá»…n. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://www.midaco' target='_blank'><img src='https://lh3.googleusercontent.com/-PtnZvoH8VJ4/Vvt_ilT5f7I/AAAAAAAAAqY/SDeyzgt0jVA_gadWGBJO1_y7ZseO1HRnwCCo/s800-Ic42/nha-bep-mau-trang.jpg' width='500px'/>nhunhu</a>. test <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
